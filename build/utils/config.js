716bfb754f2f9c99dd6f32cdc050b1a91040d56a
krishneup <queryhow@gmail.com>
2022-01-04 09:10:45 +0545

    removed nodes and build files

--- build/utils/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    port: 5000,
-    dburl: "mongodb://localhost:27017/mytest"
-};
-exports.default = config;
